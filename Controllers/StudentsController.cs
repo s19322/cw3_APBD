3441c7d5bcc1c69c55fdf28bf4bf410a06cadcd8
s19322 <61825512+s19322@users.noreply.github.com>
2020-03-19 23:40:05 +0100

    zaczete zadanie 7

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using zajecia3.Models;
 
 namespace zajecia3.Controllers
 {
@@ -14,7 +15,7 @@ namespace zajecia3.Controllers
         [HttpGet]
         public string GetStudents(string orderBy)//metoda przekazujaca dane QueryString
         {
-            return $"Natalia, Hania, Maciek sortowanie={orderBy}";
+            return $"Natalia, Hania, Maciek sortowanie={ orderBy }";
         }
 
         [HttpGet("{id}")]
@@ -23,21 +24,29 @@ namespace zajecia3.Controllers
             if (id == 1)
             {
                 return Ok("Kowalski");
-            }else if (id == 2)
+            } else if (id == 2)
             {
                 return Ok("Malewski");
             }
             return NotFound("Nie znaleziono studenta");
+         }
 
 
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
 
+        [HttpPut("id")]
 
-            [HttpPost]
+        public IActionResult UpdateStudent(int id)
+        {
+            System.Console.WriteLine("aktualizacja dokonczona");
+            return Ok(id);
 
+        }
 
-
-
-
-        
     }
 }
