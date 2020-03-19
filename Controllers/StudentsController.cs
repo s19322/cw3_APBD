5d564a94d2d1d46aafd3a3cb3bfbbbc5b823d6ed
s19322 <61825512+s19322@users.noreply.github.com>
2020-03-19 12:07:03 +0100

    Dodaj pliki projektów.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace zajecia3.Controllers
+{
+
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents(string orderBy)//metoda przekazujaca dane QueryString
+        {
+            return $"Natalia, Hania, Maciek sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+
+
+
+
+            [HttpPost]
+
+
+
+
+
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace zajecia3.Models
+{
+
+    public class Student
+    {
+        public int IdStudent { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string IndexNumber { get; set; }
+    }
+
+
+
+}
